8b2a7f0c14b46585e6a6fc2fc2f0448c0e516e0e
Evan Torkos <etorkos@gmail.com>
2015-02-05 13:13:41 -0500

    added error functionality

--- app.js
+++ app.js
@@ -11,9 +11,29 @@ app.listen(3000, function() {
 })
 
 app.set('views', './views');
+app.set('view engine', 'html');
 
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static('public'));
 app.use('/', routes);
+
+
+app.use(function(req,res,next){
+	var err = new Error("Not Found");
+	err.status = 404;
+	next(err);
+})
+
+app.use(function(err, req, res, next){
+	res.status(err.status || 500);
+	console.log({error: err});
+	res.render('error', {
+		message: err.message,
+		error: err.status
+	});
+})
 
 
 module.exports = app;
